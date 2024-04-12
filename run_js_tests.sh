cb18e4ddbab9e1ebfd94c0b0a25f2d02ee3744ca
hotpocket <githotpocket@gmail.com>
2024-04-11 18:29:28 -0700

    refactor tests into seperate files

---
+++ run_js_tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo 'JAVASCRIPT TESTS';echo; npx jest --verbose
---
+++ run_python_tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo 'PYTHON TESTS';echo; python3 ./calculate_average_grade/python_version/test_calculate_average_grade.py
